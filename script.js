c06ba55a1a1ae083f2e04ad7ad5d593f5b4d3f0b
Kriston Gooden <30561739+askforkris@users.noreply.github.com>
2023-09-24 09:20:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bios = [
+lconst bios = [
     {
         text: "Bio for Astro:Bio for Astro: Astro's future depends on the American economy. Set in the 3001. His family must decide whether to relocate because of financial difficulties.",
         epubPath: '3001.epub'
@@ -13,7 +13,7 @@ const bios = [
         epubPath: 'Duel_intercept.epub'
     },
     {
-        text: "Bio for Image 4: In "Unlocking Time: Secrets of the Unknown," journey into a world where the boundaries of reality are constantly shifting, and the doors to the past and future are waiting to be opened. This gripping narrative unfolds as a thrilling blend of science fiction, mystery, and intrigue, taking readers on an exhilarating ride through the uncharted territories of time.
+        text: "Bio for duel_2: In "Unlocking Time: Secrets of the Unknown," journey into a world where the boundaries of reality are constantly shifting, and the doors to the past and future are waiting to be opened. This gripping narrative unfolds as a thrilling blend of science fiction, mystery, and intrigue, taking readers on an exhilarating ride through the uncharted territories of time.
 
 Jonathan, a brilliant scientist, stumbles upon an astonishing discovery—a way to manipulate time itself. As he delves deeper into the mysteries of time travel, he finds himself entangled in a web of secrets, espionage, and potential extraterrestrial encounters. The keys to unlocking the truth lie within the confines of his research, but they come at a cost.
 
